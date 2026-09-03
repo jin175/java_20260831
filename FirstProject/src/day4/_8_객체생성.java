2d9949424e50919fecaf17b2fbded77bbaa982d5
jin175 <mithras11@naver.com>
2026-09-03 20:13:51 +0900

    Refactor main method in _8_객체생성.java
    Removed unnecessary comments and simplified the main method.

--- FirstProject/src/day4/_8_객체생성.java
+++ FirstProject/src/day4/_8_객체생성.java
@@ -4,32 +4,20 @@ public class _8_객체생성 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 첫글자가 대문자(약속) => 클래스
-		
-		// 정보처리기사(산업기사)
-		// sqld (데이터베이스 다루는 방법) - 노랭이책
-		// 리눅스마스터2급
-		
-		int num = 10;
 		Human hong = new Human();
 		hong.name = "홍길동";
 		
 		Human kim = new Human();
 		kim.name = "김철수";
-		
 		Human park = new Human();
-		
 		
 		System.out.println(hong.name); // 홍길동
 		System.out.println(kim.name); // 김철수
 		
-		
 		hong.eat();
 		kim.eat();
-		
-		hong.walk(1.0);
+		hong.walk(1);
 		
 		
 	}
-
 }
